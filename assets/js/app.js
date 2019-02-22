1c84d19ffea9bb54c594d52265aa6c9960883783
Igor Spasić <igor@jodd.org>
2019-02-22 13:22:42 +0100

    manu

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,18 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
+
+// adds a class
+function addClass(el, className) {
+	if (el.classList)
+		el.classList.add(className);
+	else
+		el.className += ' ' + className;
+}
+
+function removeClass(el, className) {
+	if (el.classList)
+		el.classList.remove(className);
+	else
+		el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
